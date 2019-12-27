c844cdce16b929145cce07ec3424c761f0bb0325
LinjianLi <linjiann.li@gmail.com>
2019-12-26 19:09:08 -0800

    Modify main.cpp Rename variable and modify comment.

--- main.cpp
+++ main.cpp
@@ -7,24 +7,24 @@ int main() {
     const int total = 10000;
 
     /*
-     * Define a progress bar that has a total of 100,
+     * Define a progress bar that has a total of 10000,
      * a width of 70, shows `=` to indicate completion
      * and a blank space for incomplete
      */
-    ProgressBar progressBar(total, 70, '#', '-');
+    ProgressBar pb(total, 70);
 
     for (int i = 0; i < total; i++) {
-        ++progressBar; // record the tick
+        ++pb; // record the tick
 
         usleep(200); // simulate work
 
         // display the bar only at certain steps
         if (i % 10 == 0)
-            progressBar.display();
+            pb.display();
     }
 
     // tell the bar to finish
-    progressBar.done();
+    pb.done();
 
     std::cout << "Done!" << std::endl;
     return 0;
